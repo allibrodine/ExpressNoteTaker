3c09599b94e22ed8bc8d97344b8bb987efcc1147
allibrdoine <allibrodine81@gmail.com>
2022-08-07 11:38:46 -0500

    heroku

--- Develop/Routes/htmlRoutes.js
+++ Develop/Routes/htmlRoutes.js
@@ -5,12 +5,12 @@ const app = express();
 
 //route to notes page
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 //route to index page
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 //export routes
--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const app = express();
 //server
 const PORT = process.env.PORT || 3008;
 //link to routes
-const htmlRoutes = require('./Develop/Routes/htmlRoutes');
-const apiRoutes = require('./Develop/Routes/apiRoutes');
+const htmlRoutes = require('/Develop/Routes/htmlRoutes');
+const apiRoutes = require('/Develop/Routes/apiRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //middleware functions
-app.use(express.static('./develop/public'));
+app.use(express.static('/develop/public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
